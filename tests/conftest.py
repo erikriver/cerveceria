5d5f43b9cace1e85ab0764ec38062214ab58a846
Erik Rivera <erik.river@gmail.com>
2024-09-26 15:49:18 -0600

    Preparing the tests environment

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from datetime import datetime, timedelta
+from app.models.domain import Beer, Order, Round, RoundItem
+from app.models.schemas import BeerSchema, OrderSchema, RoundSchema, RoundItemSchema, OrderItemSchema
+
+@pytest.fixture
+def sample_beers():
+    return [
+        Beer(id=1, name="Corona", price=115, quantity=50),
+        Beer(id=2, name="Quilmes", price=120, quantity=30),
+        Beer(id=3, name="Club Colombia", price=110, quantity=40),
+    ]
