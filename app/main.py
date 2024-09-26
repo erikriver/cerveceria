896a2c6c072aac2b84d2730808b06d39b5e20570
Erik Rivera <erik.river@gmail.com>
2024-09-26 14:47:30 -0600

    Set up basic FastAPI configuration

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-#
+from fastapi import FastAPI
+from app.api.routes import router
+from app.core.config import engine, Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router)
