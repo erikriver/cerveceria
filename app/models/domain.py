1f38900100c9d82db83ba7f24af4f494d4e05a06
Erik Rivera <erik.river@gmail.com>
2024-09-26 15:59:32 -0600

    Removed a SQLalchemy warning with deprecated declarative_base location

--- app/models/domain.py
+++ app/models/domain.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy.sql import func
 
 Base = declarative_base()
