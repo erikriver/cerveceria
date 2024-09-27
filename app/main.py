a4e1ed0ea60a18ed0ce3760357890334ec7eed5d
Erik Rivera <erik.river@gmail.com>
2024-09-27 04:08:27 -0600

    Added CORS middleware to the endpoints

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.routes import router
 from app.core.config import engine, Base
 from app.init_db import init_db
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 init_db()
